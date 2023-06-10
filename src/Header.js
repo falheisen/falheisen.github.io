729ff2e9479c0b94790e54f7563c4b66ffb6b3a7
falheisen <filipe.penna.soares@gmail.com>
2023-06-10 15:31:53 +0200

    update structure

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import './Header.css';
+
+function Header() {
+
+  return (
+
+    <div className="Header">
+
+      <div className='Header-title'>
+        Filipe Penna Ceravolo Soares
+      </div>
+
+      <div>
+        Teste 2
+      </div>
+    
+    </div>
+
+  );
+}
+
+export default Header;
---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import './Navbar.css';
+
+function Navbar() {
+
+  return (
+
+    <div className="Header">
+
+      <div className='Header-title'>
+        Filipe Penna Ceravolo Soares
+      </div>
+
+      <div>
+        Teste 2
+      </div>
+    
+    </div>
+
+  );
+}
+
+export default Navbar;
